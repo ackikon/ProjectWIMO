0bcd6a5f5535ae4fef221abfc3ae2f5e08a8d926
Jake <m-tabola@mail.ru>
2020-04-20 12:06:40 +0300

    Jake 20.04.2020 - add WIMOFuncWindow and file structure refractoring

--- View/Pages/WIMOMainWindowPages/LoginPage.xaml.cs
+++ View/Pages/WIMOMainWindowPages/LoginPage.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TestNewDesign.View.MainWindowSystem.LoginPage
+namespace TestNewDesign.View.Pages.WIMOMainWindowPages
 {
     /// <summary>
     /// Логика взаимодействия для LoginPage.xaml
